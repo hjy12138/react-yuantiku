eaf2abf19c25ee88c882a13df00159ec125ebf50
hjy <1271992828@qq.com>
2022-04-19 23:29:58 +0800

    day3.2

--- src/components/Tabbar/Tabbar.js
+++ src/components/Tabbar/Tabbar.js
@@ -8,28 +8,66 @@ import fast from "@/assets/images/tabbar/fast.png";
 import my_2 from "@/assets/images/tabbar/my_2.png";
 import my_1 from "@/assets/images/tabbar/my_1.png";
 import './Tabbar.less'
+import { useHistory } from 'react-router-dom';
 
 const useStyles = makeStyles({
   root: {
-    width: 500,
+    width: "100%",
   },
 });
 
 export default function Tabbar() {
+  const history = useHistory;
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
+  
+  
+
+  React.useEffect(() => {
+    // useEffect是异步的
+    switch (history.location.pathname) {
+      case "/home":
+        setValue(0);
+        break;
+      case "/fast":
+        setValue(1);       
+        break;
+      case "/user":
+        setValue(2);
+        
+        break;
+      default:
+        setValue(0);
+        
+        break;
+    }
+  }, [history.location.pathname]);  // 检测路由的更新
 
   return (
     <BottomNavigation
       value={value}
       onChange={(event, newValue) => {
         setValue(newValue);
+        switch(newValue){
+            case 0:
+                history.push("/home");
+                break;
+            case 1:
+                history.push("/fast");
+                break; 
+            case 2:
+                history.push("/user");
+                break;   
+            default:
+                break;
+            
+        }
       }}
       showLabels
       className={classes.root}
     >
-      <BottomNavigationAction label="首页" icon={<img src={value===0? home_1:home_2} />} />
-      <BottomNavigationAction label="快速刷题" icon={<img src={fast} />} />
+      <BottomNavigationAction label="首页" icon={<img src={value===0? home_1:home_2}  />} />
+      <BottomNavigationAction label="快速刷题" icon={<img src={fast} />}  />
       <BottomNavigationAction label="我的" icon={<img src={value===2? my_1:my_2} />} />
     </BottomNavigation>
   );
