0f4bf95222a6105369146d10f75926029c160485
hjy <1271992828@qq.com>
2022-04-20 10:06:49 +0800

    day4 首页完成

--- src/components/Tabbar/Tabbar.js
+++ src/components/Tabbar/Tabbar.js
@@ -8,29 +8,32 @@ import fast from "@/assets/images/tabbar/fast.png";
 import my_2 from "@/assets/images/tabbar/my_2.png";
 import my_1 from "@/assets/images/tabbar/my_1.png";
 import './Tabbar.less'
-import { useHistory } from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles({
   root: {
-    width: "100%",
+    width: 500,
   },
 });
 
 export default function Tabbar() {
-  const history = useHistory;
+  const history = useHistory();
   const classes = useStyles();
   const [value, setValue] = React.useState(0);
-  
-  
 
+  // 在赋值给value之前，我们需要先做判断，根据当前的路由地址，来决定到底myindex等于多少
+  // componentDidMount
+  // 检测的变化对象是路由地址
   React.useEffect(() => {
     // useEffect是异步的
     switch (history.location.pathname) {
       case "/home":
         setValue(0);
+        
         break;
       case "/fast":
-        setValue(1);       
+        setValue(1);
+        
         break;
       case "/user":
         setValue(2);
@@ -42,33 +45,31 @@ export default function Tabbar() {
         break;
     }
   }, [history.location.pathname]);  // 检测路由的更新
-
   return (
     <BottomNavigation
       value={value}
       onChange={(event, newValue) => {
         setValue(newValue);
-        switch(newValue){
-            case 0:
-                history.push("/home");
-                break;
-            case 1:
-                history.push("/fast");
-                break; 
-            case 2:
-                history.push("/user");
-                break;   
-            default:
-                break;
-            
+        switch (newValue) {
+          case 0:
+            history.push("/home");
+            break;
+          case 1:
+            history.push("/fast");
+            break;
+          case 2:
+            history.push("/user");
+            break;
+          default:
+            break;
         }
       }}
       showLabels
       className={classes.root}
     >
-      <BottomNavigationAction label="首页" icon={<img src={value===0? home_1:home_2}  />} />
-      <BottomNavigationAction label="快速刷题" icon={<img src={fast} />}  />
+      <BottomNavigationAction label="首页" icon={<img src={value===0? home_1:home_2} />} />
+      <BottomNavigationAction label="快速刷题" icon={<img src={fast} />} />
       <BottomNavigationAction label="我的" icon={<img src={value===2? my_1:my_2} />} />
     </BottomNavigation>
   );
-}
+} 
